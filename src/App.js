8085bc8d21bf12720636cd647e1fdde0a1f2f631
vaibhav-gulati <vaibhavgulati750@gmail.com>
2023-08-01 22:15:57 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
-import './App.css';
 import StatsTable from './components/StatsTable.js'
 
 function App() {
   return (
-    <div className="App">
+    <div style={{textAlign: 'center'}}>
      <StatsTable property="Flavanoids" />
+     {/* Here we are passing property as a prop, so that for every property there is no need to create a new component every time.
+     Similarly we can pass Hue and perform statical operations on that property */}
      {/* <StatsTable property="Hue" /> */}
      <StatsTable property="Gamma" />
     </div>
